7e2e75d8e1c65f3c7b76c9f423e165bad8363a07
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-05-15 22:16:52 +0900

    feat : create Member class

---
+++ src/main/java/com/passage/management/domain/Member.java
@@ -0,0 +1,43 @@
+package main.java.com.passage.management.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long memberId;
+    private String name;
+    private String address;
+    private String phone;
+    private String id;
+    private String password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
