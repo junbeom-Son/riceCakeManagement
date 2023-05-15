00a401c794f2ccca823fcb3b217ae20a3c0f3a1a
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-05-15 23:17:00 +0900

    chore : modified package location and added requiredArgsConstructor annotation

--- src/main/java/com/passage/management/service/MemberService.java
+++ src/main/java/com/passage/management/service/MemberService.java
@@ -1,13 +1,15 @@
 package com.passage.management.service;
 
-import main.java.com.passage.management.domain.Member;
+import com.passage.management.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     public Member registerMember(Member member) {
         member.setPassword(passwordEncoder.encode(member.getPassword()));
