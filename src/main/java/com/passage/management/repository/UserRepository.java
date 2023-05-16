0f824d84857be0eab827fb27027d5567ee0a82f2
unknown <junbeom247@gmail.com>
2023-05-16 19:24:48 +0900

    feat : implement login feature

---
+++ src/main/java/com/passage/management/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.passage.management.repository;
+
+import com.passage.management.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByLoginIdAndPassword(String loginId, String password);
+}
--- src/main/java/com/passage/management/service/UserService.java
+++ src/main/java/com/passage/management/service/UserService.java
@@ -1,18 +1,32 @@
 package com.passage.management.service;
 
 import com.passage.management.domain.User;
+import com.passage.management.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public User registerMember(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return user;
+    }
+
+    public User login(String loginId, String password) {
+        return userRepository.findByLoginIdAndPassword(loginId, password);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
