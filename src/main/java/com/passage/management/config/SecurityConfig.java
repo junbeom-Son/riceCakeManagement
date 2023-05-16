d016f28db000c5ce9147bc79d635963bc81c7591
unknown <junbeom247@gmail.com>
2023-05-16 15:23:07 +0900

    feat : jwt token 생성 및 발급 기본 작성

---
+++ src/main/java/com/passage/management/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.passage.management.config;
+
+
+
+import com.passage.management.jwt.JwtAuthenticationFilter;
+import com.passage.management.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/test").authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/users/**").hasRole("USER")
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+}
