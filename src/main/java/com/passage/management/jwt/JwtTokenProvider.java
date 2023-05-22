e4d567f39bbc9b42cc0bbb5c3935036b6e83d569
unknown <junbeom247@gmail.com>
2023-05-22 20:34:31 +0900

    fix : modify JwtTokenProvider

--- src/main/java/com/passage/management/jwt/JwtTokenProvider.java
+++ src/main/java/com/passage/management/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.passage.management.jwt;
 
+import com.passage.management.domain.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -22,6 +23,7 @@ import java.util.List;
 @Component
 public class JwtTokenProvider {
 
+    @Value("${jwt.secret}")
     private String secretKey;
     private long tokenValidTime = 30 * 60 * 1000L; // 토큰 유효시간 30분
 
@@ -29,24 +31,30 @@ public class JwtTokenProvider {
 
 
     @PostConstruct
-    protected void init(@Value("${jwt.secret}") String secretKey) {
+    protected void init() {
         // 객체 초기화, secretKey를 Base64로 인코딩
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
     // JWT 토큰 생성
-    public String createToken(Long userPk, List<String> roles) { // String -> Long으로 pk변경
-        // JWT payload에 저장되는 정보단위, 보통 여기서 user를 식별하는 값을 넣는다.
-        Claims claims = Jwts.claims().setSubject(userPk.toString());
-        claims.put("roles", roles);
+    public String createToken(Long userPk, Role role) { // String -> Long으로 pk변경
+
         Date now = new Date();
+        // JWT payload에 저장되는 정보단위, 보통 여기서 user를 식별하는 값을 넣는다.
+        Claims claims = Jwts.claims()
+                .setSubject("access_token")
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime));
+
+        // private claims
+        claims.put("info", userPk.toString());
+        claims.put("role", role);
+
         return Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + tokenValidTime)) // setExpiration Time
+                .setHeaderParam("typ", "JWT") // header
+                .setClaims(claims) // payload
                 .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘과 signature에 들어갈 secret 값 세팅
                 .compact();
-
     }
 
     // JWT 토큰에서 인증 정보 조회
@@ -57,7 +65,7 @@ public class JwtTokenProvider {
 
     // 토큰에서 회원 정보 추출
     public String getUserPk(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return (String) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("info");
     }
 
     // Request의 Header에서 token 값을 가져옵니다. "Authorization" : "TOKEN 값"
