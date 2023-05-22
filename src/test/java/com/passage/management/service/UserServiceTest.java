7462ba82db45767bbceb0331f35ff57572dc8d4c
unknown <junbeom247@gmail.com>
2023-05-22 20:06:48 +0900

    test : test new method to create token

--- src/test/java/com/passage/management/service/UserServiceTest.java
+++ src/test/java/com/passage/management/service/UserServiceTest.java
@@ -3,12 +3,21 @@ package com.passage.management.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.passage.management.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class UserServiceTest {
@@ -51,5 +60,39 @@ class UserServiceTest {
         User user = userService.login(loginId, password);
         assertNotNull(user);
         assertTrue(passwordEncoder.matches(password, user.getPassword()));
+    }
+
+    @Test
+    void createTokenTest() {
+
+        //Header 부분 설정
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        //payload 부분 설정
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.put("KEY", "HelloWorld");
+        payloads.put("NickName", "Glenn");
+        payloads.put("Age", 30);
+        payloads.put("TempAuth", "Y");
+
+        Long expiredTime = 1000 * 60L * 60L * 1L; // 토큰 유효시간 1시간
+
+        Date date = new Date();
+        date.setTime(date.getTime() + expiredTime); // 토큰 만료시간
+
+        String secretKey = "secretKeysecretKeysecretKeysecretKeysecretKey";
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        String jwt = Jwts.builder()
+                .setHeader(headers) // Headers 설정
+                .setClaims(payloads) // Claims 설정
+                .setSubject("Test") // 토큰 용도
+                .setExpiration(date) // 토큰 만료 시간 설정
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact(); // 토큰 생성
+
+        System.out.println("jwt: " + jwt);
     }
 }
